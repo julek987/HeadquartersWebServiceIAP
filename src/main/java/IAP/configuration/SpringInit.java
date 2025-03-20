eeca2fdd70c3cb84f7c60f3a8e6a75e0b88d0ede
Juliusz Szymajda <julekszymajda@gmail.com>
2025-03-20 16:42:39 +0100

    Initial commit Hello world endpoint

---
+++ src/main/java/IAP/configuration/SpringInit.java
@@ -0,0 +1,19 @@
+package IAP.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Configuration
+public class SpringInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SpringConfiguration.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    protected String[] getServletMappings() {return new String[]{"/"};}
+
+}
